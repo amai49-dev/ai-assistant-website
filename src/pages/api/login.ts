5718a5d8c875028f994b56ecbfff89a61b913213
Alastear <42664787+Alastear@users.noreply.github.com>
2025-12-16 15:33:27 +0700

    update

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -34,14 +34,22 @@ export default async function handler(
         });
 
         const data = await response.json();
+        //         exp resp from login api 
+        //         {
+        //   "success": true,
+        //   "data": {
+        //     "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJpYXQiOjE3NjU4NzM4NjksImV4cCI6MTc2NjA4OTg2OX0.SJ3VMdvmWpbjcQcYWsQyj5rWNvnDcPx7Fm-9gmnFfuI"
+        //   },
+        //   "message_th": null,
+        //   "message_en": null
+        // }
 
         if (!response.ok) {
             res.status(response.status).json({ message: 'Login failed', error: data.message });
             return;
         }
-
-        // Set token to cookie for use in middleware
-        res.setHeader('Set-Cookie', serialize('token', data.token, {
+        // set access_token to cookie name token
+        res.setHeader('Set-Cookie', serialize('token', data.data.access_token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'lax',
@@ -49,7 +57,8 @@ export default async function handler(
             maxAge: 60 * 60 * 24 * 7, // 7 days
         }));
 
-        res.status(200).json({ message: 'Login successful', token: data.token });
+        res.status(200).json({ message: 'Login successful' });
+
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: 'An unexpected error occurred' });
     }
